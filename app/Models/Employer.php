9958c78a5211197a298c10f8361e0f4022bbe65f
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2026-01-27 10:09:00 -0300

    feat: Create Employer model, migration and relationships

---
+++ app/Models/Employer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Employer extends Model
+{
+    protected $fillable = [
+        'company_name'
+    ];
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(JobListing::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JobListing extends Model
 {
@@ -67,5 +68,10 @@ class JobListing extends Model
                 fn($q) =>
                 $q->where('salary', '<=', (int) $filters['max_salary'])
             );
+    }
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
     }
 }
